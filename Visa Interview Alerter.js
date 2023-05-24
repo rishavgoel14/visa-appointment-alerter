9bbb65fc47c4483ee75c756d24f58a79ea4697c5
Rishav Goel <rishavgoel14@gmail.com>
2023-05-23 17:11:32 -0700

    Voices the location & appointment date for every change

--- Visa Interview Alerter.js
+++ Visa Interview Alerter.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var refreshCount = parseInt(sessionStorage.getItem("refreshCount")) || 0;
     console.log("This works!" + refreshCount);
     var beepAudio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+    //beepAudio.play();
     setInterval(
         function(){
             window.location.reload();
@@ -30,9 +31,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var appointmentDate = parseAppointmentDate(row.cells[1].innerText);
         if (appointmentDate != "Invalid Date" && appointmentDate != sessionStorage.getItem(location)) {
             console.log(location + ":" + appointmentDate);
+            var newLocation = location;
+            var newAppointmentDate = appointmentDate.toDateString();
             beepAudio.play();
+            setTimeout(function() {
+                var utterance = new SpeechSynthesisUtterance(newLocation + ":" + newAppointmentDate);
+                speechSynthesis.speak(utterance);
+            }, 2*1000);
         }
-        if (["Vancouver","Calgary"].includes(location) && appointmentDate.getMonth() > 6 && appointmentDate.getMonth() < 9) {
+        if (["Vancouver","Calgary"].includes(location) && appointmentDate.getTime() > 1690790400000 && appointmentDate.getTime() < 1696060800000) {
             setInterval(
                 function(){
                     beepAudio.play();
@@ -41,7 +48,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
         sessionStorage.setItem(location, appointmentDate);
     };
-    //if (refreshCount % 51 == 0) beepAudio.play();
+    //sessionStorage.setItem("playAudio",true);
+    if (sessionStorage.getItem("playAudio") == 'true') {
+        beepAudio.play();
+        sessionStorage.setItem("playAudio",'false');
+        location = "Vancouver";
+        appointmentDate = new Date();
+        setTimeout(function() {
+            var utterance = new SpeechSynthesisUtterance(location + ":" + appointmentDate.toDateString());
+            speechSynthesis.speak(utterance);
+        }, 2*1000);
+    }
     sessionStorage.setItem("lastRefreshTime", new Date());
 });
 
